167b316b3b86538ef6020549d8bbe6cf6f8f52d8
Scott Boulter <Scott.Boulter@transformingsupport.uk>
2021-11-26 15:41:48 +0000

    Significant codebase update

--- main.js
+++ main.js
@@ -1,65 +1,78 @@
-var fullName = document.querySelector(".employee");
-var mileageData = document.querySelector(".mileagedata");
-var list = document.querySelector(".employeelist");
-var addEmployee = document.querySelector(".btnEmployee");
-var deleteEmployees = document.querySelector(".btnDeleteAll");
-var employeeList = [];
+var employeeName = document.querySelector('#input-name');
+var employeeMileage = document.querySelector('#input-mileage');
+var employeeForm = document.querySelector('#form');
+var employeeListed = document.querySelector('#mileage-tracker');
+var employeeDelete = document.querySelector("#delete-all");
+var employeeArray = JSON.parse(localStorage.getItem('mileage-tracker')) || [];
 
-function getData(elements) {
-  list.innerHTML = "";
-  elements.forEach(e => {
-    let newElement = document.createElement("li");
-    newElement.innerHTML = e;
-    newElement.classList.add("list-group-item");
-    let deleteButton = document.createElement("button");
-    deleteButton.innerHTML = "Delete";
-    deleteButton.classList.add("btn-primary");
-    deleteButton.classList.add("deleteButton");
-    list.appendChild(deleteButton);
-    list.appendChild(newElement);
-
-    deleteButton.onclick = function() {
-      if (confirm('Are you sure you want to delete this record?')) {
-      newElement.remove();
-      this.remove();
-      localStorage.setItem("mylist", JSON.stringify(employeeList));
-      }
-      else {
-        return;
-      }
-    };
-  });
+function updateTracker(){
+  employeeListed.innerHTML = '';
+  for (var toBeDeleted in employeeArray) {
+    var li = document.createElement('li');
+    li.classList.add('list-group-item');
+    var span = document.createElement('span');
+    span.innerText = employeeArray[toBeDeleted];
+    var deleteButton = document.createElement('button');
+    deleteButton.innerText = 'Delete';
+    deleteButton.setAttribute('toBeDeleted',toBeDeleted); 
+    deleteButton.classList.add('delete');
+    deleteButton.classList.add('btn-primary');
+    deleteButton.classList.add('delete-employee');
+    li.appendChild(span);
+    li.appendChild(deleteButton);
+    employeeListed.appendChild(li);
+  }
+  localStorage.setItem('mileage-tracker',JSON.stringify(employeeArray));
 }
 
-addEmployee.addEventListener("click", e => {
-  if (fullName.value !== "" && mileageData.value !== "") {
-    employeeList.unshift(fullName.value + " has claimed back on " + mileageData.value + " miles this tax year");
-    fullName.value = "";
-    mileageData.value = "";
-    getData(employeeList);
-    deleteEmployees.style.display = "block";
-    localStorage.setItem("mylist", JSON.stringify(employeeList));
-  } else {
-    alert("Please input some data");
+function addToTracker(value){
+  if (employeeName.value === "" || employeeMileage.value === "") return alert("Please input some data");
+    employeeArray.unshift(employeeName.value + " has claimed back on " + employeeMileage.value + " miles this tax year");
+    updateTracker();
+    employeeName.value = "";
+    employeeMileage.value = "";
+}
+
+function deleteFromTracker(toBeDeleted){
+  if (confirm("Are you sure you want to delete this record?")) {
+    employeeArray.splice(Number(toBeDeleted),1);
+    updateTracker();
+    employeeName.value = "";
+    employeeMileage.value = "";
+  }
+  else {
+    return;
+  }
+};
+
+employeeForm.addEventListener('submit', event => {
+  event.preventDefault();
+  addToTracker(employeeName.value && employeeMileage.value);
+  checkRecordTotal();
+});
+
+document.addEventListener('click', event => {
+  var element = event.target;
+  if (element.classList.contains('delete')){ 
+    deleteFromTracker(element.getAttribute('toBeDeleted'));
+    checkRecordTotal();
   }
 });
 
-let saved = localStorage.getItem("mylist");
-if (saved) {
-  employeeList = JSON.parse(localStorage.getItem("mylist"));
-  getData(employeeList);
-} else {
-  deleteEmployees.style.display = "none";
+employeeDelete.addEventListener('click', function() {
+  if (confirm("Are you sure you want to delete all records?")) {
+    localStorage.clear();
+    location.reload();
+  }
+  else {
+    return;
+  }
+});
+
+function checkRecordTotal() {
+  for (var i = 0; i < employeeArray.length; i++);
+  document.getElementById('records').innerHTML = ("Showing " + i + " record(s)");
 }
 
-deleteEmployees.addEventListener("click", function() {
-if (confirm('Are you sure you want to delete all records?')) {
-  localStorage.clear();
-  list.innerHTML = "";
-  employeeList = [];
-  deleteEmployees.style.display = "none";
-}
-else {
-  return;
-}
-});
+updateTracker();
+checkRecordTotal();
